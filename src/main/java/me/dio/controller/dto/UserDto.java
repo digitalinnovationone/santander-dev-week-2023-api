472c506e46c44490885f7b19f0a6a1d631436a8d
Venilton FalvoJr <falvojr@gmail.com>
2023-07-14 21:41:17 -0300

    Include ID field into User DTO

--- src/main/java/me/dio/controller/dto/UserDto.java
+++ src/main/java/me/dio/controller/dto/UserDto.java
@@ -7,6 +7,7 @@ import java.util.List;
 import me.dio.domain.model.User;
 
 public record UserDto(
+        Long id,
         String name, 
         AccountDto account, 
         List<FeatureDto> features, 
@@ -19,11 +20,12 @@ public record UserDto(
         var cardsDto = model.getCards().stream().map(CardDto::fromModel).collect(toList());
         var newsDto = model.getNews().stream().map(NewsDto::fromModel).collect(toList());
 
-        return new UserDto(model.getName(), accountDto, featuresDto, cardsDto, newsDto);
+        return new UserDto(model.getId(), model.getName(), accountDto, featuresDto, cardsDto, newsDto);
     }
 
     public User toModel() {
         User model = new User();
+        model.setId(this.id);
         model.setName(this.name);
         model.setAccount(this.account.toModel());
         model.setFeatures(this.features.stream().map(FeatureDto::toModel).collect(toList()));
