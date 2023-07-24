b1ef30fea300b48abb3617bf3b50476f80c33089
Venilton FalvoJr <falvojr@gmail.com>
2023-07-24 11:35:08 -0300

    Enable CORS to Angular Client

--- src/main/java/me/dio/controller/UserController.java
+++ src/main/java/me/dio/controller/UserController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/users")
 @Tag(name = "Users Controller", description = "RESTful API for managing users.")
