8350d84f14362ebbfb045031316dbd46fbe213c0
Venilton FalvoJr <falvojr@gmail.com>
2023-07-25 09:06:27 -0300

    Fix to prevent writes on User with ID 1

--- src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -15,6 +15,12 @@ import static java.util.Optional.ofNullable;
 @Service
 public class UserServiceImpl implements UserService {
 
+    /**
+     * ID de usuário utilizado na Santander Dev Week 2023.
+     * Por isso, vamos criar algumas regras para mantê-lo integro.
+     */
+    private static final Long UNCHANGEABLE_USER_ID = 1L;
+
     private final UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
@@ -37,6 +43,7 @@ public class UserServiceImpl implements UserService {
         ofNullable(userToCreate.getAccount()).orElseThrow(() -> new BusinessException("User account must not be null."));
         ofNullable(userToCreate.getCard()).orElseThrow(() -> new BusinessException("User card must not be null."));
 
+        this.validateChangeableId(userToCreate.getId(), "created");
         if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
             throw new BusinessException("This account number already exists.");
         }
@@ -48,6 +55,7 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     public User update(Long id, User userToUpdate) {
+        this.validateChangeableId(id, "updated");
         User dbUser = this.findById(id);
         if (!dbUser.getId().equals(userToUpdate.getId())) {
             throw new BusinessException("Update IDs must be the same.");
@@ -64,8 +72,15 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     public void delete(Long id) {
+        this.validateChangeableId(id, "deleted");
         User dbUser = this.findById(id);
         this.userRepository.delete(dbUser);
+    }
+
+    private void validateChangeableId(Long id, String operation) {
+        if (UNCHANGEABLE_USER_ID.equals(id)) {
+            throw new BusinessException("User with ID %d can not be %s.".formatted(UNCHANGEABLE_USER_ID, operation));
+        }
     }
 }
 
